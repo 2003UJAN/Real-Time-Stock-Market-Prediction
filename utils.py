cba5dd42bfbe7f84fd7201476318eaa6385ee245
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-05-02 10:05:54 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 
-def plot_predictions(real, predicted):
-    plt.figure(figsize=(10,5))
-    plt.plot(real, label='Actual')
-    plt.plot(predicted, label='Predicted')
+def plot_predictions(real, predicted, title="Actual vs Predicted", xlabel="Time", ylabel="Stock Price"):
+    plt.figure(figsize=(10, 5))
+    plt.plot(real, label='Actual', marker='o')
+    plt.plot(predicted, label='Predicted', marker='x')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.legend()
+    plt.grid(True)
     return plt
